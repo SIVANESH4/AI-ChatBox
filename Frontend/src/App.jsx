34050bce8704edda48315e6b5b39052762cdb102
SIVANESH4 <sivanesh6363@gmail.com>
2026-02-24 20:27:16 +0530

    Included Some Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import ChatPage from './ChatPage'
+import LoginPage from './LoginPage'
+
 
 function App() {
   return (
-    <></>
+    <LoginPage/>
   )
 }
 
